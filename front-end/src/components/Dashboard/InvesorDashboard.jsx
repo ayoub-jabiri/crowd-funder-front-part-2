4bb93ac4e56628de5afb03853461932cf508b251
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-06-04 17:10:39 +0100

    Arranged a custom router for the investor dashboard

--- front-end/src/components/Dashboard/InvesorDashboard.jsx
+++ front-end/src/components/Dashboard/InvesorDashboard.jsx
@@ -1,3 +1,21 @@
+import Card from "./Card";
+
 export default function InvesorDashboard() {
-    return <div>investor</div>;
+    return (
+        <div className="dashboard">
+            <div className="page-header">
+                <header>
+                    <h1 className="text-xl font-bold">Dashboard Page</h1>
+                </header>
+
+                <section className="stats">
+                    <Card
+                        title="Total Projects"
+                        value={stats.total}
+                        subtitle="All projects"
+                    />
+                </section>
+            </div>
+        </div>
+    );
 }
