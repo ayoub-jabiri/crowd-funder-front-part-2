9b6a4eded957e98cc10ed7c0c51734ff4e9b3ebb
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-06-06 23:29:43 +0100

    Set up the wallet deposit modal

---
+++ front-end/src/components/wallet/DepositModal.jsx
@@ -0,0 +1,8 @@
+export default function DepositModal() {
+    return (
+        <div>
+            <h1 className="text-xl font-bold">Deposit Modal</h1>
+            <p>This is where the deposit form will go.</p>
+        </div>
+    );
+}
