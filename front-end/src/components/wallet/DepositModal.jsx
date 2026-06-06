73d3554b4043efba36cb7402cd5f9737e8e4209c
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-06-07 00:19:16 +0100

    Finished the deposit modal design

--- front-end/src/components/wallet/DepositModal.jsx
+++ front-end/src/components/wallet/DepositModal.jsx
@@ -1,8 +1,41 @@
-export default function DepositModal() {
+import { RiCloseLine } from "@remixicon/react";
+
+export default function DepositModal({ closeModal }) {
     return (
-        <div>
-            <h1 className="text-xl font-bold">Deposit Modal</h1>
-            <p>This is where the deposit form will go.</p>
+        <div
+            id="default-modal"
+            className="bg-[#000000d6] overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen flex justify-center items-center"
+        >
+            <div className="relative p-4 w-full max-w-2xl max-h-full">
+                <div className="relative bg-white text-black border border-default rounded-base shadow-sm p-4 md:p-6 rounded-md">
+                    <div className="flex items-center justify-between border-b border-default pb-4 md:pb-5">
+                        <h3 className="text-lg font-medium text-heading">
+                            Deposit
+                        </h3>
+                        <button
+                            type="button"
+                            className="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center cursor-pointer"
+                            onClick={() => closeModal()}
+                        >
+                            <RiCloseLine />
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <div className="space-y-4 md:space-y-6 py-4 md:py-6">
+                        <input
+                            type="number"
+                            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-[300px] px-3 py-2.5 mx-auto shadow-xs placeholder:text-body"
+                            placeholder="Amount"
+                        />
+                        <button
+                            type="button"
+                            className="block bg-white text-black border border-black font-medium leading-5 rounded-md text-sm px-4 py-2.5 mx-auto cursor-pointer"
+                        >
+                            <span>Deposit</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
